8b5cd89a84899a5bbd85dc886d5cf32d4457f2e2
Ryohei Ikegami <iofg2100@gmail.com>
2020-03-01 16:52:15 +0900

    Add video capture

--- index.ts
+++ index.ts
@@ -6,3 +6,15 @@ const canvas = document.getElementById('canvas')
         console.log('lerp result: ' + (Module as any).lerp(1, 2, 0.5));
     }
 }
+
+;(async () => {
+    const video = document.getElementById('video') as HTMLVideoElement
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          width: { ideal: video.width },
+          height: { ideal: video.height },
+        },
+    })
+    video.srcObject = stream
+})()
