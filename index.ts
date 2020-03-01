832f1e2f12dd5c3a9572680a15dd651155f70e34
Ryohei Ikegami <iofg2100@gmail.com>
2020-03-01 16:37:15 +0900

    Call C++ function

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 const canvas = document.getElementById('canvas')
 
 ;(window as any).Module = {
-    canvas: canvas
+    canvas: canvas,
+    onRuntimeInitialized: () => {
+        console.log('lerp result: ' + (Module as any).lerp(1, 2, 0.5));
+    }
 }
