3bbbb2a5be8f3f76cf35e35bb3dde5dc1fd738ec
Ryohei Ikegami <iofg2100@gmail.com>
2020-03-01 17:21:06 +0900

    Reformat

--- index.ts
+++ index.ts
@@ -1,34 +1,34 @@
 const canvas = document.getElementById('canvas')
 
 ;(window as any).Module = {
-    canvas: canvas,
-    onRuntimeInitialized: init,
+  canvas: canvas,
+  onRuntimeInitialized: init,
 }
 
 async function init() {
-    const video = document.createElement('video')
-    video.width = 320
-    video.height = 240
-    video.autoplay = true
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-          width: { ideal: video.width },
-          height: { ideal: video.height },
-        },
-    })
-    video.srcObject = stream
+  const video = document.createElement('video')
+  video.width = 320
+  video.height = 240
+  video.autoplay = true
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      width: { ideal: video.width },
+      height: { ideal: video.height },
+    },
+  })
+  video.srcObject = stream
 
-    const videoCanvas = document.createElement('canvas')
-    videoCanvas.width = video.width
-    videoCanvas.height = video.height
-    const context = videoCanvas.getContext('2d')!
+  const videoCanvas = document.createElement('canvas')
+  videoCanvas.width = video.width
+  videoCanvas.height = video.height
+  const context = videoCanvas.getContext('2d')!
 
-    const updateCanvas = () => {
-        context.drawImage(video, 0, 0)
-        const data = context.getImageData(0, 0, videoCanvas.width, videoCanvas.height)
-        ;(window as any).Module.putImageData(data.data)
-        requestAnimationFrame(updateCanvas)
-    }
-    updateCanvas()
+  const updateCanvas = () => {
+    context.drawImage(video, 0, 0)
+    const data = context.getImageData(0, 0, videoCanvas.width, videoCanvas.height)
+    ;(window as any).Module.putImageData(data.data)
+    requestAnimationFrame(updateCanvas)
+  }
+  updateCanvas()
 }
