fb13494e25f806a806c98f2c006211075ab09671
Ryohei Ikegami <iofg2100@gmail.com>
2020-04-14 17:50:52 +0900

    Speed up

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,66 @@
 interface EmscriptenModule {
-  detectAndRender(data: number, width: number, height: number): void
+  detectAndRender(data: number, width: number, height: number): void;
 }
 
 const preRun = () => {
-  FS.createPreloadedFile('/', 'shape_predictor_68_face_landmarks.dat', 'shape_predictor_68_face_landmarks.dat', true, false)
-  FS.createPreloadedFile('/', 'deeplabv3_257_mv_gpu.tflite', 'deeplabv3_257_mv_gpu.tflite', true, false)
-}
+  FS.createPreloadedFile(
+    "/",
+    "shape_predictor_68_face_landmarks.dat",
+    "shape_predictor_68_face_landmarks.dat",
+    true,
+    false
+  );
+  FS.createPreloadedFile(
+    "/",
+    "deeplabv3_257_mv_gpu.tflite",
+    "deeplabv3_257_mv_gpu.tflite",
+    true,
+    false
+  );
+};
 
 const init = async () => {
-  const video = document.createElement('video')
-  video.width = 320
-  video.height = 240
+  const video = document.createElement("video");
+  video.width = 200;
+  video.height = 150;
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: {
       width: { ideal: video.width },
       height: { ideal: video.height },
     },
-  })
-  video.srcObject = stream
-  video.play()
+  });
+  video.srcObject = stream;
+  video.play();
 
-  const videoCanvas = document.createElement('canvas')
-  videoCanvas.width = video.width
-  videoCanvas.height = video.height
-  const context = videoCanvas.getContext('2d')!
+  const videoCanvas = document.createElement("canvas");
+  videoCanvas.width = video.width;
+  videoCanvas.height = video.height;
+  const context = videoCanvas.getContext("2d")!;
 
   const updateCanvas = () => {
-    context.drawImage(video, 0, 0)
-    const data = context.getImageData(0, 0, videoCanvas.width, videoCanvas.height)
+    context.drawImage(video, 0, 0);
+    const data = context.getImageData(
+      0,
+      0,
+      videoCanvas.width,
+      videoCanvas.height
+    );
 
-    const buffer = Module._malloc(data.data.length)
+    const buffer = Module._malloc(data.data.length);
     Module.HEAPU8.set(data.data, buffer);
-    Module.detectAndRender(buffer, data.width, data.height)
-    Module._free(buffer)
+    Module.detectAndRender(buffer, data.width, data.height);
+    Module._free(buffer);
 
-    requestAnimationFrame(updateCanvas)
-  }
-  updateCanvas()
+    requestAnimationFrame(updateCanvas);
+  };
+  updateCanvas();
 
-  document.getElementById('loading')!.hidden = true
-}
+  document.getElementById("loading")!.hidden = true;
+};
 
 window.Module = {
-  canvas: document.getElementById('canvas'),
+  canvas: document.getElementById("canvas"),
   preRun: preRun,
   onRuntimeInitialized: init,
-} as any
+} as any;
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <SDL/SDL.h>
-#include <emscripten.h>
-#include <emscripten/bind.h>
 #include <dlib/array2d.h>
-#include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_processing.h>
+#include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_transforms/draw.h>
 #include <dlib/opencv.h>
+#include <emscripten.h>
+#include <emscripten/bind.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
 #include <tensorflow/lite/interpreter.h>
@@ -13,12 +13,12 @@
 #include <tensorflow/lite/model.h>
 
 class FaceDetector {
- public:
+public:
   FaceDetector() {
     dlib::deserialize("/shape_predictor_68_face_landmarks.dat") >> poseModel;
   }
 
-  std::vector<cv::Rect2d> detectTrackFaces(const cv::Mat& bgrImage) {
+  std::vector<cv::Rect2d> detectTrackFaces(const cv::Mat &bgrImage) {
     if (tracker && trackableCount > 0) {
       cv::Rect2d outRect;
       if (tracker->update(bgrImage, outRect)) {
@@ -27,21 +27,25 @@ class FaceDetector {
       }
     }
     tracker.reset();
-    std::vector<dlib::rectangle> faces = faceDetector(dlib::cv_image<dlib::bgr_pixel>(bgrImage));
+    std::vector<dlib::rectangle> faces =
+        faceDetector(dlib::cv_image<dlib::bgr_pixel>(bgrImage));
     if (faces.empty()) {
       return {};
     }
     auto face = faces.at(0);
     cv::Rect2d rect(face.left(), face.top(), face.width(), face.height());
-    tracker = cv::TrackerKCF::create();
+    tracker = cv::TrackerMedianFlow::create();
     tracker->init(bgrImage, rect);
     trackableCount = 20;
     return {rect};
   }
 
-  std::vector<cv::Point2d> detectLandmarks(const cv::Mat& bgrImage, const cv::Rect2d& face) {
-    dlib::rectangle dlibFace(face.tl().x, face.tl().y, face.br().x, face.br().y);
-    dlib::full_object_detection shape = poseModel(dlib::cv_image<dlib::bgr_pixel>(bgrImage), dlibFace);
+  std::vector<cv::Point2d> detectLandmarks(const cv::Mat &bgrImage,
+                                           const cv::Rect2d &face) {
+    dlib::rectangle dlibFace(face.tl().x, face.tl().y, face.br().x,
+                             face.br().y);
+    dlib::full_object_detection shape =
+        poseModel(dlib::cv_image<dlib::bgr_pixel>(bgrImage), dlibFace);
     std::vector<cv::Point2d> points(shape.num_parts());
     for (int i = 0; i < shape.num_parts(); ++i) {
       points[i] = cv::Point2d(shape.part(i).x(), shape.part(i).y());
@@ -49,7 +53,7 @@ class FaceDetector {
     return points;
   }
 
- private:
+private:
   dlib::frontal_face_detector faceDetector = dlib::get_frontal_face_detector();
   dlib::shape_predictor poseModel;
   std::shared_ptr<cv::Tracker> tracker;
@@ -57,9 +61,10 @@ class FaceDetector {
 };
 
 class PersonSegmenter {
- public:
+public:
   PersonSegmenter() {
-    _model = tflite::FlatBufferModel::BuildFromFile("/deeplabv3_257_mv_gpu.tflite");
+    _model =
+        tflite::FlatBufferModel::BuildFromFile("/deeplabv3_257_mv_gpu.tflite");
     tflite::ops::builtin::BuiltinOpResolver resolver;
     tflite::InterpreterBuilder(*_model, resolver)(&_interpreter);
     _interpreter->SetNumThreads(1);
@@ -68,20 +73,22 @@ class PersonSegmenter {
     }
   }
 
-  cv::Mat segment(const cv::Mat& bgrImage) {
+  cv::Mat segment(const cv::Mat &bgrImage) {
     cv::Mat input;
     cv::resize(bgrImage, input, cv::Size(_inputSize, _inputSize));
     cv::cvtColor(input, input, cv::COLOR_BGR2RGB);
     input.convertTo(input, CV_32FC3, 1 / 255.0);
 
-    auto inputData = _interpreter->typed_tensor<float>(_interpreter->inputs().at(0));
+    auto inputData =
+        _interpreter->typed_tensor<float>(_interpreter->inputs().at(0));
     memcpy(inputData, input.data, sizeof(float) * 3 * _inputSize * _inputSize);
 
     if (_interpreter->Invoke() != kTfLiteOk) {
       std::cerr << "Error Invoke" << std::endl;
     }
 
-    auto outputData = _interpreter->typed_tensor<float>(_interpreter->outputs()[0]);
+    auto outputData =
+        _interpreter->typed_tensor<float>(_interpreter->outputs()[0]);
     cv::Mat output(_inputSize, _inputSize, CV_8UC1);
 
     for (int i = 0; i < _inputSize * _inputSize; ++i) {
@@ -97,7 +104,7 @@ class PersonSegmenter {
     return output;
   }
 
- private:
+private:
   std::unique_ptr<tflite::FlatBufferModel> _model;
   std::unique_ptr<tflite::Interpreter> _interpreter;
   constexpr static int _inputSize = 257;
@@ -105,15 +112,15 @@ class PersonSegmenter {
 
 namespace {
 
-constexpr int WIDTH = 320;
-constexpr int HEIGHT = 240;
-SDL_Surface* screen = nullptr;
+constexpr int WIDTH = 200;
+constexpr int HEIGHT = 150;
+SDL_Surface *screen = nullptr;
 FaceDetector faceDetector;
 PersonSegmenter personSegmenter;
 
-}
+} // namespace
 
-extern "C" int main(int argc, char** argv) {
+extern "C" int main(int argc, char **argv) {
 
   SDL_Init(SDL_INIT_VIDEO);
   screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_SWSURFACE);
@@ -130,31 +137,31 @@ void detectAndRender(size_t addr, int width, int height) {
   auto faces = faceDetector.detectTrackFaces(bgrImage);
   std::vector<std::vector<cv::Point2d>> landmarkss;
 
-  for (auto&& face : faces) {
+  for (auto &&face : faces) {
     auto landmarks = faceDetector.detectLandmarks(bgrImage, face);
     landmarkss.push_back(std::move(landmarks));
-
-
   }
 
-  //bgrImage = personSegmenter.segment(bgrImage);
+  // bgrImage = personSegmenter.segment(bgrImage);
 
-  for (auto&& face : faces) {
+  for (auto &&face : faces) {
     cv::rectangle(bgrImage, face, cv::Scalar(255, 0, 0));
   }
-  for (auto&& landmarks : landmarkss) {
-    for (auto&& p : landmarks) {
+  for (auto &&landmarks : landmarkss) {
+    for (auto &&p : landmarks) {
       cv::circle(bgrImage, p, 2, cv::Scalar(0, 255, 0));
     }
   }
 
-  if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
+  if (SDL_MUSTLOCK(screen))
+    SDL_LockSurface(screen);
   cv::Mat dstRGBAImage(height, width, CV_8UC4, screen->pixels);
   cv::cvtColor(bgrImage, dstRGBAImage, cv::COLOR_BGR2RGBA);
-  if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
-  SDL_Flip(screen); 
+  if (SDL_MUSTLOCK(screen))
+    SDL_UnlockSurface(screen);
+  SDL_Flip(screen);
 }
 
 EMSCRIPTEN_BINDINGS(my_module) {
-    emscripten::function("detectAndRender", &detectAndRender);
+  emscripten::function("detectAndRender", &detectAndRender);
 }
